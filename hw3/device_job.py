777bb51b3d5e3038372511f3a5b09c6bcc1a6b6d
AlexandrBon <bon527870@mail.ru>
2023-12-30 03:32:26 +0600

    add checkpoint settings

--- hw3/device_job.py
+++ hw3/device_job.py
@@ -1,12 +1,15 @@
 from pyflink.common import SimpleStringSchema
+from pyflink.common import Configuration
 from pyflink.common.typeinfo import Types, RowTypeInfo
 from pyflink.common.watermark_strategy import WatermarkStrategy
-from pyflink.datastream import StreamExecutionEnvironment, TimeCharacteristic
+from pyflink.datastream import StreamExecutionEnvironment, TimeCharacteristic, \
+    CheckpointingMode, ExternalizedCheckpointCleanup
 from pyflink.datastream.connectors import DeliveryGuarantee
 from pyflink.datastream.connectors.kafka import KafkaSource, \
     KafkaOffsetsInitializer, KafkaSink, KafkaRecordSerializationSchema
 from pyflink.datastream.formats.json import JsonRowDeserializationSchema
 from pyflink.datastream.functions import MapFunction
+from pyflink.datastream.checkpoint_config import CheckpointConfig
 
 
 def python_data_stream_example():
@@ -16,7 +19,37 @@ def python_data_stream_example():
     # assertion.
     env.set_parallelism(1)
     env.set_stream_time_characteristic(TimeCharacteristic.EventTime)
-    
+
+    # start a checkpoint every 1000 ms
+    env.enable_checkpointing(1000)
+
+    # advanced options:
+
+    # set mode to exactly-once (this is the default)
+    env.get_checkpoint_config().set_checkpointing_mode(CheckpointingMode.EXACTLY_ONCE)
+
+    # make sure 500 ms of progress happen between checkpoints
+    env.get_checkpoint_config().set_min_pause_between_checkpoints(500)
+
+    # checkpoints have to complete within one minute, or are discarded
+    env.get_checkpoint_config().set_checkpoint_timeout(60000)
+
+    # only two consecutive checkpoint failures are tolerated
+    env.get_checkpoint_config().set_tolerable_checkpoint_failure_number(2)
+
+    # allow only one checkpoint to be in progress at the same time
+    env.get_checkpoint_config().set_max_concurrent_checkpoints(1)
+
+    # enable externalized checkpoints which are retained after job cancellation
+    env.get_checkpoint_config().enable_externalized_checkpoints(ExternalizedCheckpointCleanup.RETAIN_ON_CANCELLATION)
+
+    # enables the unaligned checkpoints
+    env.get_checkpoint_config().enable_unaligned_checkpoints()
+
+    config = CheckpointConfig()
+    config.set_checkpoint_storage("file:///opt/pyflink/tmp/checkpoints/logs")
+    config.set_checkpoint_storage_dir("tmp")
+    env.configure(config)
 
     type_info: RowTypeInfo = Types.ROW_NAMED(['device_id', 'temperature', 'execution_time'],
                                              [Types.LONG(), Types.DOUBLE(), Types.INT()])
